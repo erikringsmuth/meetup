8826386523985c6e7537297b687b37836d78db54
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2013-07-22 22:58:04 -0500

    getting mad at backbone models and collections

--- static/js/backboneApp/people/peopleView.js
+++ static/js/backboneApp/people/peopleView.js
@@ -3,8 +3,9 @@ define([
 	'underscore',
 	'backbone',
 	'./peopleCollection',
+	'./personModel',
 	'text!./peopleTemplate.html'
-], function($, _, Backbone, PeopleCollection, peopleTemplate) {
+], function($, _, Backbone, PeopleCollection, PersonModel, peopleTemplate) {
 	'use strict';
 	
 	return Backbone.View.extend({
@@ -12,12 +13,33 @@ define([
 		
 		collection: new PeopleCollection(),
 		
+		events: {
+			'click button[name="create"]': 'createPerson',
+			'click button[name="delete"]': 'deletePerson'
+		},
+		
 		render: function() {
 			var that = this;
 			this.collection.fetch({success: function(collection, response, options) {
-				that.$el.html(that.template({model: response}));
+				that.$el.html(that.template({model: collection.models}));
 			}});
 			return this;
+		},
+		
+		createPerson: function(event) {
+			var $createButton = $(event.currentTarget);
+			new PersonModel({
+				name: $createButton.siblings('[name="name"]').val(),
+				username: $createButton.siblings('[name="username"]').val()
+			}).save();
+			this.render();
+		},
+		
+		deletePerson: function(event) {
+			var $deleteButton = $(event.currentTarget);
+			var person = this.collection.get($deleteButton.data('cid'));
+			person.destroy();
+			this.render();
 		}
 	});
 });
